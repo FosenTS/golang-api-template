57214fb2ebd66b9b438f6277124827f92d8edc77
ivan kruglov <juster050@yandex.com>
2024-03-18 00:10:20 +0300

    replace deprecated funtions

--- pkg/qrgen/qrcode.go
+++ pkg/qrgen/qrcode.go
@@ -53,7 +53,6 @@ import (
 	"image/color"
 	"image/png"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 
@@ -379,7 +378,7 @@ func (q *QRCode) WriteFile(size int, filename string) error {
 		return err
 	}
 
-	return ioutil.WriteFile(filename, png, os.FileMode(0644))
+	return os.WriteFile(filename, png, os.FileMode(0644))
 }
 
 // encode completes the steps required to encode the QR Code. These include
